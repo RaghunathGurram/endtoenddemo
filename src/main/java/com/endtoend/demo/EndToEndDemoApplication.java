605d1fe71d4c52e66bebaefa906a005b1a16a653
RaghunathGurram <46337575+RaghunathGurram@users.noreply.github.com>
2022-02-22 22:12:59 +0530

    second commit

--- src/main/java/com/endtoend/demo/EndToEndDemoApplication.java
+++ src/main/java/com/endtoend/demo/EndToEndDemoApplication.java
@@ -1,14 +1,9 @@
 package com.endtoend.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
-//@ComponentScan(basePackages = {"com.endtoend.demo.controller"})
 public class EndToEndDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/endtoend/demo/controller/DemoController.java
+++ src/main/java/com/endtoend/demo/controller/DemoController.java
@@ -1,15 +1,13 @@
 package com.endtoend.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
-//@CrossOrigin(origins = "*")
 public class DemoController {
-	
+
 	@GetMapping("/print")
 	public String print() {
 		return "Welcome to End To End Demo";
